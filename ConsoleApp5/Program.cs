5bd0a7c5ecf3488cee834105ed07fa51a4e042ba
4free69 <45002325+4free69@users.noreply.github.com>
2018-12-24 19:25:00 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            int sum = 0;
+            while(i <= 2)
+            {
+                for(int x = 2; ; x++)
+                {
+                    for (int y = x - 1; y >= 1; y--)
+                    {
+                        if (x % y == 0)
+                            sum = sum + y;
+                        continue;
+                    }
+                    if(sum == x)
+                    {
+                        Console.Write(x + ",");
+                        i++;
+                    }
+                    sum = 0;
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
